1ef7df2ef9984d40dcc7b5aeed9fb0d63c49edda
Mykola Nazimkov <nazimkoff@gmail.com>
2022-11-27 16:33:54 +0200

    intro to JS

---
+++ script.js
@@ -0,0 +1,9 @@
+// змінні (переменные) - це спеціальні комірки в яких можна зберігати данні
+// типи данних - строки та числа
+var first = 'Hello world'
+let second = 'Hello world 2'
+const third =  2022
+
+var firstNumber = prompt('Введіть перше число')
+var secondNumber = prompt('Введіть друге число')
+alert()
